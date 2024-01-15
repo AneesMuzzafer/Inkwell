7eafe9b5df03b4cfcf823675c8e2480db6c1daf2
Anees Muzzafer <aneesmuzzafer@gmail.com>
2024-01-15 11:50:13 +0530

    🩹 Fix Category view

--- app/views/Category.php
+++ app/views/Category.php
@@ -1,5 +1,5 @@
 
-<section class="compose" style="flex-direction: column; padding: 0 var(--px); border: 0px; height: calc(100vh - 140px);">
+<section class="compose" style="flex-direction: column; padding: 0 var(--px); border: 0px; min-height: calc(100vh - 140px);">
     <div style="font-size: 24px; font-weight: 600; padding: 20px; color: var(--text-gray); ">All Categories</div>
     <div class="category-container" >
 
@@ -14,9 +14,9 @@
     <form class="write-container" style="border: none;" method="post" action="/category">
 
         <label for="name" style="margin-top: 2rem;">Name</label>
-        <input required type="text" id="name" name="name" class="input-field" placeholder="Enter your title">
+        <input required type="text" id="name" name="name" class="input-field" placeholder="Category name">
 
-        <div style="text-align: right;">
+        <div style="text-align: right; margin-bottom: 20px;">
             <button type="submit" class="button-outlined">Add Category</button>
         </div>
     </form>
